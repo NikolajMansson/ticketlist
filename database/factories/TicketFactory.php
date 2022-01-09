365ea99b1a6e4386cf1ce30baa4497406569ab06
Nikolaj Mansson <nikolaj.mansson@gmail.com>
2022-01-09 13:55:48 +0000

    Ticket model, object and factory

---
+++ database/factories/TicketFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+use Faker\Generator as Faker;
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+$factory->define(App\Ticket::class, function(Faker $faker)
+    {
+        return [
+            'title' => $faker->text(50),
+	'comment' => $faker->text(200),
+	'open' => $faker->boolean()
+        ];
+    });
