591187ef5f31cb166f565b7ed17b7c23baa74a05
Nikolaj Mansson <nikolaj.mansson@gmail.com>
2022-01-10 11:54:29 +0000

    Added author field to factory

--- database/factories/TicketFactory.php
+++ database/factories/TicketFactory.php
@@ -8,6 +8,7 @@ $factory->define(App\Ticket::class, function(Faker $faker)
         return [
             'title' => $faker->text(50),
 	'comment' => $faker->text(200),
+	'author' => $faker->text(100),
 	'open' => $faker->boolean()
         ];
     });
